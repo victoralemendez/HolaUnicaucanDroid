23b7d272d737d54f94ecd339e6359b1940b95bf2
A Méndez <amendeza97@gmail.com>
2019-09-26 19:38:35 -0500

    Second commit

--- app/src/main/java/edu/unicauca/holaunicaucandroid/MainActivity.java
+++ app/src/main/java/edu/unicauca/holaunicaucandroid/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //FIXME arreglar
         //TODO falta por hacer algo
+        //Mejorando
     }
 }
