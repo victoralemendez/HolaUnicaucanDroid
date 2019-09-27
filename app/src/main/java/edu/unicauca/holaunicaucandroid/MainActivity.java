a6c1ca5813df043257e22867e71fc755e3dd228c
A Méndez <amendeza97@gmail.com>
2019-09-26 19:35:02 -0500

    Initial commit

---
+++ app/src/main/java/edu/unicauca/holaunicaucandroid/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.unicauca.holaunicaucandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //FIXME arreglar
+        //TODO falta por hacer algo
+    }
+}
